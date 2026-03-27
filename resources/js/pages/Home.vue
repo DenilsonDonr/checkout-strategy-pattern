cdcfbf6a74b5c3d5793b1b3550a5ab85db11674c
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-27 04:17:33 +0000

    Remove Inertia home route from web.php

--- resources/js/pages/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Hola desde vue + Inertia</h1>
-    </div>
-</template>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
 
 Route::prefix('checkout')->name('checkout.')->group(function () {
     Route::get('/',        [CheckoutController::class, 'index'])->name('index');
